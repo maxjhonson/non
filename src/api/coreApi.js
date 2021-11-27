2eab7ba89ae7b274415189cf19ae30d9a9002fed
Maxwell Jhonson <max.jhonson07@gmail.com>
2021-11-27 12:45:01 -0400

    bug creating new questionaire

--- src/api/coreApi.js
+++ src/api/coreApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://consultateapi.herokuapp.com",
 });
 
 //https://consultateapi.herokuapp.com/
