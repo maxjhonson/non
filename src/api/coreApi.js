ca4a247740a4d165568019b8697436f7a81736c3
Maxwell Jhonson <max.jhonson07@gmail.com>
2021-12-21 08:37:47 -0400

    -

--- src/api/coreApi.js
+++ src/api/coreApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://consultateapi.herokuapp.com",
+  baseURL: "https://consultateapi.herokuapp.com/",
 });
 
 //https://consultateapi.herokuapp.com/
